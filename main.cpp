71decb518f503e10458dc7aade78ed9096b5ca23
Michael Vogt <michaelvogt@muenster.de>
2026-04-30 11:56:25 +0200

    Qt6 integration

--- main.cpp
+++ main.cpp
@@ -1,24 +1,11 @@
-#include <iostream>
+#include <QApplication>
+#include <QWidget>
 
-#include "geometry/Line.h"
-#include "src/geometry/LinearObject.h"
-#include "src/geometry/Point.h"
-#include "src/geometry/Scene.h"
-
-int main() {
-    Scene scene;
-
-    Point p1(0, 0);
-    Point p2(3, 4);
-    Line L1(&p1, &p2);
-    Line L2(1, 2, 3);
-
-    std::cout << L2.toString() << std::endl;
-
-    std::cout << L1.toString() << std::endl;
-    p2.moveTo(6, 9);
-    p1.moveTo(0, 1);
-    std::cout << L1.toString() << std::endl;
-
-    return 0;
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    QWidget window;
+    window.resize(400,300);
+    window.setWindowTitle("Stoicheia");
+    window.show();
+    return app.exec();
 }
